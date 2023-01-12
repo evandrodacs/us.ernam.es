e4ab30eb8a3f2020d6ccb3a657da80b94e6fb9eb
evandrodacs <122316717+evandrodacs@users.noreply.github.com>
2023-01-12 00:13:56 -0300

    add actions add services include proxy

--- app.lua
+++ app.lua
@@ -1,8 +1,72 @@
+local gsub = string.gsub
+
 local lapis = require("lapis")
+local csrf = require("lapis.csrf")
+local http = require("lapis.nginx.http")
+
 local app = lapis.Application()
 
-app:get("/", function()
-  return "Welcome to us.ernam.es"
+local services = require("services")
+
+for index = 1, #services do 
+  local service = services[index]
+  services[service.name] = service
+end
+
+app.layout = "layout"
+
+app:get("/", function(self)
+  self.services = {}
+  self.csrf_token = csrf.generate_token(self)
+  return {
+    render = "index"
+  }
+end)
+
+-- /check?service=name&username=&csrf_token=
+app:get("/check", function(self)
+  local link = nil
+  if csrf.validate_token(self) then 
+    if services[self.params.service] then
+      local service = services[self.params.service]
+      local success = false
+      if type(service.test) == "string" then 
+        link = gsub(service.test, "%[username%]", ngx.escape_uri(self.params.username, 0))
+        local _, status = http.simple(link)
+        if status == 200 then
+          success = true
+        end
+      elseif type(service.test) == "function" then 
+        local available = false
+        link, available = service.test(self.params.username)
+        if type(link) == "string" and available then
+          link = gsub(link, "%[username%]", ngx.escape_uri(self.params.username, 0))
+          success = true
+        end
+      end
+      if success then
+        if type(service.link) == "string" then
+          link = gsub(service.link, "%[link%]", link)
+        end
+        return {
+          json = {
+            service = service.name,
+            available = true,
+            link = link
+          }
+        }
+      end
+    end
+  end 
+  if type(link) ~= "string" then
+    link = nil
+  end
+  return {
+    json = {
+      service = service.name,
+      link = link or ""
+    }
+  } 
 end)
 
 return app
---
+++ services.lua
@@ -0,0 +1,54 @@
+local http = require("lapis.nginx.http")
+
+local get = function(url, body, headers)
+    return http.simple({
+        url = url,
+        method = "GET",
+        headers = headers
+    })
+end
+
+local post = function(url, body, headers)
+    return http.simple({
+        url = url,
+        method = "POST",
+        headers = headers,
+        body = body
+    })
+end
+
+return {
+
+    --[[
+
+    [position] = {
+        name = "Media name",
+
+        type = "media", -- default
+        type = "domain",
+
+        -- function test
+        test = function(username)
+            return "https://media.name/[username]", false -- (or nothing) not available
+            return "https://media.name/[username]", true -- available (pass the username link)
+        end,
+
+        -- string test (if status returns ~ 200 suceeds)
+        test = "https://media.name/[username]", -- [username] = path encoded, {username} = query encoded
+
+        link = "[link]?ref=us.ernam.es", -- if have referral link, we can use it to monetize
+
+        icon = "social.png" -- from static/icons/ folder
+    },
+
+    --]]
+
+    {
+        name = "Telegram",
+        type = "media",
+        test = "https://t.me/[username]",
+        icon = "telegram.png"
+    },
+
+
+}
