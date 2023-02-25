c44c09982b2fff084399b65ee4ebe209a086f177
evandrodacs <122316717+evandrodacs@users.noreply.github.com>
2023-02-25 11:03:46 -0300

    remove tecnosfera config

--- config.lua
+++ config.lua
@@ -7,21 +7,6 @@ config("development", {
 
   usernames = {
     url = "https://us.ernam.es/"
-  },
-
-  tecnosfera = {
-    name = "Tecnosfera",
-    url = "https://tecnosfera.io"
-    medias = {
-      txt = true,
-      pdf = true,
-      jpg = true,
-      jpeg = true,
-      png = true,
-      gif = true,
-      bmp = true,
-      zip = true,
-      rar = true
-    }
   }
+  
 })
